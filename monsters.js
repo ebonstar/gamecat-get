df2ed487b4a9653e00531c6ce9c5dd697cc5797a
ebonstar <18689574+ebonstar@users.noreply.github.com>
2022-11-27 15:52:01 +0000

    add support for monsters with multiple ranks

--- monsters.js
+++ monsters.js
@@ -1,6 +1,8 @@
 import got from "got";
 import { JSDOM } from "jsdom";
-import { getTextFromNodes } from "./utils.js";
+import { getTablesByFirstText, getTextFromNodes } from "./utils.js";
+
+const [LR, HR, MR] = ["LR", "HR", "MR"];
 
 /**
  * getMonsterName
@@ -12,25 +14,42 @@ function getMonsterName(table) {
 }
 
 /**
+ * getMonsterRank
+ * return possible monster ranks
+ */
+function getMonsterRank(table) {
+  const rank = [];
+  const cells = getTextFromNodes(table, "td:first-child");
+  if (cells.includes("Low")) rank.push(LR);
+  if (cells.includes("High")) rank.push(HR);
+  if (cells.includes("Master")) rank.push(MR);
+  return rank;
+}
+
+/**
  * getMonsterMaterials
  * return monster material info
  */
-function getMonsterMaterials(table) {
+function getMonsterMaterials(tables, ranks) {
   const materials = [];
-  const rows = Array.from(table.querySelectorAll("tbody tr"));
-  rows.forEach((row) => {
-    const cells = getTextFromNodes(row, "td");
+  tables.forEach((table, i) => {
+    const rows = [...table.querySelectorAll("tbody tr")];
+    rows.forEach((row) => {
+      const cells = getTextFromNodes(row, "td");
 
-    materials.push({
-      name: cells[1],
-      target: cells[3],
-      carve: cells[4],
-      capture: cells[5],
-      break: cells[6],
-      drop: cells[7],
-      palico: cells[8],
+      materials.push({
+        name: cells[1],
+        rank: ranks[i],
+        target: cells[3],
+        carve: cells[4],
+        capture: cells[5],
+        break: cells[6],
+        drop: cells[7],
+        palico: cells[8],
+      });
     });
   });
+
   return materials;
 }
 
@@ -40,20 +59,32 @@ function getMonsterMaterials(table) {
  */
 export async function getMonsters() {
   const response = await got(
-    "https://gamecat.fun/en/index.php?title=Monster/Astalos"
+    "https://gamecat.fun/en/index.php?title=Monster/Aknosom"
   );
 
   const dom = new JSDOM(response.body);
   const page = dom.window.document;
 
-  const monster = {};
-  const tables = page.querySelectorAll("table.wikitable.sortable");
-  tables.forEach((table) => {
-    const headers = getTextFromNodes(table, "th");
+  const tables = [...page.querySelectorAll("table.wikitable.sortable")];
 
-    if (headers.includes("Japanese")) monster.name = getMonsterName(table);
-    if (headers.includes("Material name"))
-      monster.materials = getMonsterMaterials(table);
-  });
+  const nameTable = getTablesByFirstText(tables, "English");
+  const name = getMonsterName(nameTable);
+
+  const armourTable = getTablesByFirstText(tables, "Rank");
+  const ranks = getMonsterRank(armourTable);
+
+  const materialTables = getTablesByFirstText(
+    tables,
+    "Material name",
+    "filter"
+  );
+  const materials = getMonsterMaterials(materialTables, ranks);
+
+  const monster = {
+    name,
+    ranks,
+    materials,
+    quests: [],
+  };
   console.log(monster);
 }
--- utils.js
+++ utils.js
@@ -1,3 +1,11 @@
+export function getTablesByFirstText(tables, text, type) {
+  const matches = tables.filter(
+    (table) => table.textContent.trim().search(text) === 0
+  );
+
+  return type === "filter" ? matches : matches[0];
+}
+
 /**
  * getTextFromNodes
  * given a parent node, select all matching nodes and return their text in an array
